beb5b6846899c699c27c2a67357f19e8190e5b31
Mikolaj Izdebski <mizdebsk@redhat.com>
2025-07-12 07:25:05 +0200

    Update to XMvn 5.1.0

--- dola-dbs/src/main/java/io/kojan/dola/imperator/Imperator.java
+++ dola-dbs/src/main/java/io/kojan/dola/imperator/Imperator.java
@@ -234,7 +234,7 @@ public class Imperator {
 
         String mavenGoal = ctx.isSkipTests() ? "package" : "verify";
         args.add(mavenGoal);
-        args.add("org.fedoraproject.xmvn:xmvn-mojo:5.0.0:install");
+        args.add("org.fedoraproject.xmvn:xmvn-mojo:5.1.0:install");
 
         List<String> lines = new ArrayList<>();
         lines.add("# Run the actual Maven build, executing all build plugins and so on.");
